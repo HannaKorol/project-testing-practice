4e65ffe7110d01b3edfdc2280770a115ee436b16
hak <hak@tutao.de>
2024-12-05 16:33:24 +0100

    Add test jest function

---
+++ sum.js
@@ -0,0 +1,12 @@
+/*function sum(a, b) {
+	return a + b;
+}
+module.exports = sum;*/
+
+
+function capitalize(s) {
+	return s.charAt(0).toUpperCase();
+}
+module.exports = capitalize;
+
+
---
+++ sum.test.js
@@ -0,0 +1,20 @@
+/*
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+	expect(sum(1,2)).toBe(3);
+});
+*/
+
+
+
+/*
+const capitalize = require('./sum');
+test('S the 0 character capitalized', () => {
+	expect(capitalize(s)).toBe(/S/);
+});
+*/
+
+
+
+
